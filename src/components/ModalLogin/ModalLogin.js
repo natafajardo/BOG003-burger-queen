5a79acc09d7a84013457dc60fb820fe4a0a7c7a4
Lu Valcke <lfvalcke@gmail.com>
2021-11-18 15:40:18 -0500

    Modal login y registro estilos terminados

--- src/components/ModalLogin/ModalLogin.js
+++ src/components/ModalLogin/ModalLogin.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import './ModalLogin.css'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
@@ -7,7 +8,8 @@ import Form from 'react-bootstrap/Form'
 const ModalLogin = ({ showModal, setShowModal }) => {
 
     const [valueName, setValueName] = useState('');
-    
+    const [showForm, setShowForm] = useState(false);
+
     const auth = () => {
         /* createUserWithEmailAndPassword(valueName) */
         console.log(valueName);
@@ -15,45 +17,55 @@ const ModalLogin = ({ showModal, setShowModal }) => {
 
     const handleChange = (event) => {
         setValueName(event.target.value);
-    }    
+    }
+
 
     return (
         <Modal show={showModal}>
             <Modal.Dialog>
-                <Modal.Header onClick={() => setShowModal(false)} closeButton>
-                    <Modal.Title>Modal title</Modal.Title>
-                </Modal.Header>
 
-                <Modal.Body>
+                <Modal.Body className={showForm ? "formRegisterVisible" : "formRegisterNoVisible"}>
+                    <Modal.Header onClick={() => setShowModal(false)} closeButton>
+                        <Modal.Title>Registro</Modal.Title>
+                    </Modal.Header>
                     <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicName">
                             <Form.Label>Nombre</Form.Label>
                             <Form.Control type="text" value={valueName} onChange= {handleChange} placeholder="Nombre" />
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Correo electrónico</Form.Label>
+                            <Form.Control type="email" placeholder="Correo electrónico" />
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control type="password" placeholder="Contraseña" />
                         </Form.Group>
                         <Button variant="primary" type="submit" onClick={auth} >
-                            Submit
+                            Enviar
                         </Button>
                     </Form>
+                    <p onClick={() => {setShowForm(false)}}> Si ya estás registrado haz click aquí</p>
                 </Modal.Body>
-
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={() => setShowModal(false)} >Close</Button>
-                    <Button variant="primary">Save changes</Button>
-                </Modal.Footer>
+                <Modal.Body className={showForm ? "formLoginNoVisible" : "formLoginVisible"}>
+                    <Modal.Header onClick={() => setShowModal(false)} closeButton>
+                        <Modal.Title>Ingreso</Modal.Title>
+                    </Modal.Header>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Correo electrónico</Form.Label>
+                            <Form.Control type="email" placeholder="Correo electrónico" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control type="password" placeholder="Contraseña" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={auth} >
+                        Enviar
+                        </Button>
+                    </Form>
+                    <p onClick={() => {setShowForm(true)}}> Si no estás registrado haz click aquí</p>
+                </Modal.Body>
             </Modal.Dialog>
         </Modal>
     )
